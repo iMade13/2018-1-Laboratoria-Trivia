83db0c1845a978ca63420bf24c8a918ad5590971
Madeleine Marcano <madeleine1302@gmail.com>
2018-05-18 16:04:42 -0400

    agregando caja de nombre

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
-window.onload = function() {
-    var name = prompt("¿cuál es tu nombre?");
-    document.getElementById("name").innerText = name;
+/*window.onload = function()*/
+{
+    function introducirNombre() {
+        var mensaje;
+        document.getElementById("name").innerText = name;
 
-    var wantToPlay = prompt("¿quieres jugar? s/n");
-    if (wantToPlay.toLowerCase() == "s") {
-        var answer1 = prompt("¿han egresado hombres en Laboratoria? s/n");
-        if (answer1.toLowerCase() == "n") {
-            document.getElementById("rightAnswersText").innerHTML =
-                document.getElementById("rightAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No han egresado hombres</p>"
-            "</div>";
-        } else {
-            document.getElementById("wrongAnswersText").innerHTML =
-                document.getElementById("wrongAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No han egresado hombres</p>"
-            "</div>";
-        }
+        var wantToPlay = prompt("¿quieres jugar? s/n");
+        if (wantToPlay.toLowerCase() == "s") {
+            var answer1;
+            if (answer1.toLowerCase() == "n") {
+                document.getElementById("rightAnswersText").innerHTML =
+                    document.getElementById("rightAnswersText").innerHTML +
+                    "<div class='answer'>" +
+                    "<p>No han egresado hombres</p>"
+                "</div>";
+            } else {
+                document.getElementById("wrongAnswersText").innerHTML =
+                    document.getElementById("wrongAnswersText").innerHTML +
+                    "<div class='answer'>" +
+                    "<p>No han egresado hombres</p>"
+                "</div>";
+            }
 
-        var answer2 = prompt("¿hay laboratoria en concepción? s/n");
-        if (answer2.toLowerCase() == "n") {
-            document.getElementById("rightAnswersText").innerHTML =
-                document.getElementById("rightAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No hay laboratoria en concepción</p>"
-            "</div>";
+            var answer2 = prompt("¿hay laboratoria en concepción? s/n");
+            if (answer2.toLowerCase() == "n") {
+                document.getElementById("rightAnswersText").innerHTML =
+                    document.getElementById("rightAnswersText").innerHTML +
+                    "<div class='answer'>" +
+                    "<p>No hay laboratoria en concepción</p>"
+                "</div>";
+            } else {
+                document.getElementById("wrongAnswersText").innerHTML =
+                    document.getElementById("wrongAnswersText").innerHTML +
+                    "<div class='answer'>" +
+                    "<p>No hay laboratoria en concepción</p>"
+                "</div>";
+            }
         } else {
-            document.getElementById("wrongAnswersText").innerHTML =
-                document.getElementById("wrongAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No hay laboratoria en concepción</p>"
-            "</div>";
+            document.getElementById("warningMessage").innerText = "Bueno Chao";
         }
-    } else {
-        document.getElementById("warningMessage").innerText = "Bueno Chao";
     }
-}
